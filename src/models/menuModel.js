96ddbc5ca3e96ae8a26f20bed2f3c47aec9ee6d7
kawser Bappy <bappydu2015@gmail.com>
2026-03-26 00:53:34 +0600

    Menu model added

---
+++ src/models/menuModel.js
@@ -0,0 +1,68 @@
+import mongoose from "mongoose";
+
+const menuSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    ingredients: {
+      type: String,
+      trim: true,
+    },
+    isAvailable: {
+      type: Boolean,
+      default: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    isFeatured: {
+      type: Boolean,
+      default: true,
+    },
+    variants: [
+      {
+        name: { type: String, required: true },
+        price: { type: Number, required: true },
+      },
+    ],
+    discount: {
+      type: Number,
+      default: 0,
+      min: 0,
+      max: 100,
+    },
+    tags: [{ type: String }],
+    prepTime: {
+      type: Number,
+      default: 20,
+    },
+    rating: { type: Number, default: 0 },
+    numReviews: { type: Number, default: 0 },
+  },
+  { timestamps: true },
+);
+
+const Menu = mongoose.model("Menu", menuSchema);
+export default Menu;
